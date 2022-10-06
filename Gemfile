ea44d0fb519ba4925c34e05cf7228e3fb84adaa9
Marc Busqué <marc@lamarciana.com>
2022-10-06 16:14:34 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in aixo_es_una_prova.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/aixo_es_una_prova.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "aixo_es_una_prova/version"
+
+module AixoEsUnaProva
+  class Error < StandardError; end
+  # Your code goes here...
+end
